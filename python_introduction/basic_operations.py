864e49a4afd4280996eac56a207f91ddd08fd3fd
Michael <mikelogunsoluwaseyi@gmail.com>
2025-05-20 21:59:51 +0100

    Python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtraction = number1 + number2
+multiplication = number1 * number2
+print(f"addition of 10 and 5 is 15")
+print(f"subtractio0n of 10 and 5 is 5")
+print(f"multiplication of 10 and 5 is 50")
